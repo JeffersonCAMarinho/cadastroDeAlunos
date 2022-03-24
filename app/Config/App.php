4a2b19357c3fe13e224875b3e0e49e8782a7930f
Jefferson Marinho <jeffersonmarinho97@gmail.com>
2022-03-24 17:16:10 -0300

    fix

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/project-root/public/';
+    public $baseURL = 'http://localhost/cadastroDeAlunos/public/';
 
     /**
      * --------------------------------------------------------------------------
